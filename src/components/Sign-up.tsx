a62abe84f79c41405db5f225d1d8e651de5f0cc5
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-04-03 15:13:11 -0400

    added route from log-in to MarsTrip page

--- src/components/Sign-up.tsx
+++ src/components/Sign-up.tsx
@@ -1,86 +1,96 @@
-import { useState } from "react"
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../LogInSignUp.css";
 import { toast } from "react-toastify";
 
 export function SignUp() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const navigate = useNavigate();
-
-    const signUpError = () => toast.error("Invalid email or password", {
-        position: "top-right",
-        autoClose: 900,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
+  const signUpError = () =>
+    toast.error("Invalid email or password", {
+      position: "top-right",
+      autoClose: 900,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
     });
 
-    const passwordLengthError = () => toast.error("Password must be at least 8 characters", {
-        position: "top-right",
-        autoClose: 900,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
+  const passwordLengthError = () =>
+    toast.error("Password must be at least 8 characters", {
+      position: "top-right",
+      autoClose: 900,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
     });
 
-    const passwordNoMatchError = () => toast.error("Passwords do not match", {
-        position: "top-right",
-        autoClose: 900,
-        hideProgressBar: true,
-        closeOnClick: true,
-        pauseOnHover: false,
-        draggable: true,
-        progress: undefined,
+  const passwordNoMatchError = () =>
+    toast.error("Passwords do not match", {
+      position: "top-right",
+      autoClose: 900,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: false,
+      draggable: true,
+      progress: undefined,
     });
 
-    function handleSubmit(e: any) {
+  function handleSubmit(e: any) {
+    e.preventDefault();
 
-        e.preventDefault();
-
-        if (email.length === 0 && password.length === 0) {
-            signUpError();
-            return;
-        } else if (password.length < 8 && confirmPassword.length < 8) {
-            passwordLengthError();
-        } else if (password !== confirmPassword) {
-            passwordNoMatchError();
-        } else {
-            navigate("/home");
-        }
-
+    if (email.length === 0 && password.length === 0) {
+      signUpError();
+      return;
+    } else if (password.length < 8 && confirmPassword.length < 8) {
+      passwordLengthError();
+    } else if (password !== confirmPassword) {
+      passwordNoMatchError();
+    } else {
+      navigate("/home");
     }
+  }
 
-    return (
-        <div className="loginContainer">
-            <form>
-                <h1>Sign Up</h1>
-                <label>
-                    <p>Email</p>
-                    <input type="text" onChange={e => setEmail(e.target.value)} />
-                </label>
-                <label>
-                    <p>Password</p>
-                    <input type="password" onChange={e => setPassword(e.target.value)} />
-                </label>
-                <label>
-                    <p>Confirm Password</p>
-                    <input type="password" onChange={e => setConfirmPassword(e.target.value)} />
-                </label>
-                <div>
-                    <label>Already have an account?</label>
-                    <Link className="link" to="/login">Log In</Link>
-                </div>
-                <div>
-                    <button className="button" type="submit" onClick={handleSubmit}>Submit</button>
-                </div>
-            </form>
+  return (
+    <div className="loginContainer">
+      <form>
+        <h1>Sign Up</h1>
+        <label>
+          <p>Email</p>
+          <input type="text" onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          <p>Confirm Password</p>
+          <input
+            type="password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+        <div>
+          <label>Already have an account?</label>
+          <Link className="link" to="/login">
+            Log In
+          </Link>
         </div>
-    )
+        <div>
+          <button className="button" type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { TripsBook } from "./components/Booktrip";
 import { LogIn } from "./components/Log-in";
 import { SignUp } from "./components/Sign-up";
+import { MarsTrip } from "./components/MarsTrip";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -17,9 +18,9 @@ root.render(
       <Routes>
         <Route path="/" element={<LogIn />}></Route>
         <Route path="/showTrip" element={<TripsBook></TripsBook>}></Route>
-        <Route path="/showMarsTrip" element={<TripsBook></TripsBook>}></Route>
         <Route path="/login" element={<LogIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/getTripDetails" element={<MarsTrip />} />
       </Routes>
     </Router>
   </React.StrictMode>
