48fa70d70eb816e84a19449272ee0a37f8e26b5a
ramirolynch <ramirolynch@yahoo.com>
2022-04-04 09:21:59 -0500

    connected the Log-in.tsx component to the backend

--- src/components/Log-in.tsx
+++ src/components/Log-in.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../LogInSignUp.css";
 import { toast } from "react-toastify";
+import {logIn} from '../services/SpaceTravelApi'
+import { SpaceContext } from "../context/SpaceContext";
 
 export function LogIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   let navigate = useNavigate();
+  const { loginUser } = useContext(SpaceContext)
 
   const loginError = () =>
     toast.error("Invalid email or password", {
@@ -26,24 +29,32 @@ export function LogIn() {
       loginError();
       return;
     } else {
-      navigate("/getTripDetails");
+      let formData = new FormData(e.currentTarget)
+
+      let email: string = formData.get('email') as string;
+      let password: string = formData.get('password') as string;
+
+      // checked for logged users
+      logIn(email, password).then(() => loginUser())
+      navigate("/getTripDetails")
+       
     }
   }
 
   return (
     <div className="loginContainer">
-      <form>
+      <form onSubmit={handleSubmit}>
         <h1>Are you Ready to go to Mars!?</h1>
         <h2>Log In</h2>
         <label>
           <p>Email</p>
-          <input type="text" onChange={(e) => setEmail(e.target.value)} />
+          <input type="text"  name="email" id="email"  value={email} onChange={(e) => setEmail(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
           <input
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}
           />
         </label>
 
@@ -55,7 +66,7 @@ export function LogIn() {
         </div>
 
         <div>
-          <button className="button" type="submit" onClick={handleSubmit}>
+          <button className="button" type="submit">
             Submit
           </button>
         </div>
