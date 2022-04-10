55195acde2a1d323fc86cb5f84b65af60e2fb14c
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-04-10 08:43:19 -0400

    added the boarding pass

--- src/components/Trip.tsx
+++ src/components/Trip.tsx
@@ -1,29 +1,65 @@
 import { Trip } from "../models/TripModel";
 import moment from "moment";
 import { FaRocket } from "react-icons/fa";
-import {IconContext} from "react-icons"
+import { IconContext } from "react-icons";
 import { SelectedTrip } from "./SelectedTrip";
 import { useNavigate } from "react-router-dom";
 import { SpaceContext } from "../context/SpaceContext";
 import { useContext } from "react";
+import { jsPDF } from "jspdf";
+import { IMAGE_BASE64 } from "../constants/ImageHelper";
 
 interface Props {
   trip: Trip;
 }
 
 export function SingleTrip({ trip }: Props) {
-  
-  const { addSelectedTrip } = useContext(SpaceContext)
-  
+  const { addSelectedTrip } = useContext(SpaceContext);
+
   let navigate = useNavigate();
 
-  function handleClick(id: number) {
-    addSelectedTrip(id)
-    navigate(`/trips/${id}`);
-
+  function handleClick(trip: Trip) {
+    addSelectedTrip(trip.id);
+    generatePDF(trip);
+    navigate(`/trips/${trip.id}`);
   }
 
-  
+  function generatePDF(trip: Trip) {
+    var doc = new jsPDF("l", "px", [250, 500]);
+    var imgData = "data:image/jpeg;base64," + IMAGE_BASE64;
+    doc.addImage(imgData, "jpeg", 0, 0, 510, 250);
+    doc.setTextColor("white");
+    doc.addFont("helvetica", "normal", "sans-serif", 700);
+    doc.text("BOARDING PASS", 220, 20);
+    doc.text(
+      "Departure Date :" +
+        "" +
+        moment(trip.departure_date).format("MM/DD/YYYY") +
+        "",
+      20,
+      60
+    );
+    doc.text(
+      "Arrival Date :" +
+        "" +
+        moment(trip.arrival_date).format("MM/DD/YYYY") +
+        "",
+      20,
+      80
+    );
+    doc.text("Trip time (Terrestrial Hours) :" + "" + trip.trip_time, 20, 100);
+    doc.text("Transportation Company :" + "" + trip.company_name, 20, 120);
+    doc.text("Trip Price :" + "" + trip.price + "", 20, 140);
+    doc.text("Destination :" + "" + trip.location_name + "", 20, 160);
+    doc.text(
+      "Distance :" + "" + trip.distance + " " + trip.unit_of_measure,
+      20,
+      180
+    );
+    doc.text("Space Suit Type :" + "" + trip.space_suit_name + "", 20, 200);
+    doc.save("boardingPass.pdf");
+  }
+
   return (
     <IconContext.Provider value={{ style: { marginLeft: "10px" } }}>
       <div className="TripItem">
@@ -64,7 +100,7 @@ export function SingleTrip({ trip }: Props) {
           </li>
         </ul>
 
-        <button className="liftOff" onClick={()=>handleClick(trip.id)}>
+        <button className="liftOff" onClick={() => handleClick(trip)}>
           Lift-Off
           <FaRocket />
         </button>
