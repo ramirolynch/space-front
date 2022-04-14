238c2054a9c0f4b1ce9629ea2a7e81d934646dd2
SudhaVallabhapurapu <89112048+sudhasew@users.noreply.github.com>
2022-04-14 11:42:53 -0400

    added changes to photo caption

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import "./App.css";
 import { LogIn } from "./components/Log-in";
 import { MarsTrip } from "./components/MarsTrip";
-import { PhotoCaption } from "./components/PhotoCaption";
 import { PhotoOfDay } from "./components/PhotoOfDay";
 function App() {
-
-
   return (
     <div>
-      <LogIn></LogIn>
       <PhotoOfDay></PhotoOfDay>
-      <PhotoCaption></PhotoCaption>
     </div>
   );
 }
--- src/components/PhotoOfDay.tsx
+++ src/components/PhotoOfDay.tsx
@@ -14,7 +14,19 @@ export function PhotoOfDay() {
     <div className="frame">
       <figure>
         <img src={imgSrc} alt="photo_of_day_nasa" />
-        {/* <figcaption>{caption}</figcaption> */}
+        <figcaption className="caption">
+          <h2
+            style={{
+              color: "rgba(152, 0, 109)",
+              textDecoration: "underline",
+              fontSize: "1.5rem",
+              margin: "7px 0 7px 0",
+            }}
+          >
+            Caption
+          </h2>{" "}
+          {caption}
+        </figcaption>
       </figure>
     </div>
   );
